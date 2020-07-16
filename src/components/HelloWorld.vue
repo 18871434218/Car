c3c78c1c36726728e34cc763ab9436de5ce74601
wangdong <18871434218@163.com>
2020-07-16 22:56:19 +0800

    one

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="hello">
+    <div style="text-align:center;font-size:24px; padding-top:40px"><h1>{{ msg }}</h1></div>
+    <div style="text-align:center;font-size:20px"><h1>{{ msg1 }}</h1></div>
+    
+     
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: '舜宇车联网平台',
+      msg1:"车端模拟系统"
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
